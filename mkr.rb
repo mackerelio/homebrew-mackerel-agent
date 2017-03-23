0d3b7b880766b3e35dd717d0881e9d7e992e00cb
fand <fand.gmork@gmail.com>
2017-03-23 16:40:22 +0900

    mkr 0.14.4

--- mkr.rb
+++ mkr.rb
@@ -1,12 +1,12 @@
 class Mkr < Formula
   homepage 'https://github.com/mackerelio/mkr'
-  version '0.14.3'
+  version '0.14.4'
   if Hardware::CPU.is_64_bit?
-    url 'https://github.com/mackerelio/mkr/releases/download/v0.14.3/mkr_darwin_amd64.zip'
-    sha256 'c0dca79f43b95bb45d5e19bfa2b2095721e42d6f940060543003e53de25e59b1'
+    url 'https://github.com/mackerelio/mkr/releases/download/v0.14.4/mkr_darwin_amd64.zip'
+    sha256 '17fc17a0b3aa17d30b0a1ddb49cd7c815e714c30500650edbc7118ded6b8e680'
   else
-    url 'https://github.com/mackerelio/mkr/releases/download/v0.14.3/mkr_darwin_386.zip'
-    sha256 '859a3cb976297fb46c2477c44f26ed771cb8d8066f5b5dc22a78281a1fb288f8'
+    url 'https://github.com/mackerelio/mkr/releases/download/v0.14.4/mkr_darwin_386.zip'
+    sha256 'ab093834b268b29f6034da5b4bc8772d7940e090652ac8c3fe0043973195e70c'
   end
 
   head do
