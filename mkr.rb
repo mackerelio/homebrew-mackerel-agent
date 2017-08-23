f8f4c553e5f566d09798550357e0189ae88e2daf
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-08-23 17:27:18 +0900

    ruby tools/update.rb mkr 0.17.0

--- mkr.rb
+++ mkr.rb
@@ -1,12 +1,12 @@
 class Mkr < Formula
   homepage 'https://github.com/mackerelio/mkr'
-  version '0.16.1'
+  version '0.17.0'
   if Hardware::CPU.is_64_bit?
-    url 'https://github.com/mackerelio/mkr/releases/download/v0.16.1/mkr_darwin_amd64.zip'
-    sha256 '1dd5b2343819658d22781634e12b15b26d6dbf65e1a529cec47a6219d676ecf8'
+    url 'https://github.com/mackerelio/mkr/releases/download/v0.17.0/mkr_darwin_amd64.zip'
+    sha256 '390496371732b86a0415b9452469394c8db3e0e9e941834b389638d348b9943c'
   else
-    url 'https://github.com/mackerelio/mkr/releases/download/v0.16.1/mkr_darwin_386.zip'
-    sha256 'ba206b86fac9045ea7565ec21c0fc5204bcc3dc496c57bb2b444830a6d4fb6c8'
+    url 'https://github.com/mackerelio/mkr/releases/download/v0.17.0/mkr_darwin_386.zip'
+    sha256 '963c0cf277a4ef7365c7b505059c04697a2f2372d2a82b46db0d1552a4800294'
   end
 
   head do
