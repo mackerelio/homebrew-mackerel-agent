43602e7bec17f8e747fd87d98c27c97f306bd902
Asato Wakisaka <asato.wakisaka@gmail.com>
2019-10-24 10:37:05 +0900

    stop providing 32bit binaries

--- mkr.rb
+++ mkr.rb
@@ -1,13 +1,8 @@
 class Mkr < Formula
   homepage 'https://github.com/mackerelio/mkr'
   version '0.39.2'
-  if Hardware::CPU.is_64_bit?
-    url 'https://github.com/mackerelio/mkr/releases/download/v0.39.2/mkr_darwin_amd64.zip'
-    sha256 '00adabd7fd98f66e5649b3944a4ad7255e171abb5906f010d24a6e078858e52f'
-  else
-    url 'https://github.com/mackerelio/mkr/releases/download/v0.39.2/mkr_darwin_386.zip'
-    sha256 '9c934a3f3123469f487c53fa728b86e8ccc27ae890e1d628d638a3489c926580'
-  end
+  url 'https://github.com/mackerelio/mkr/releases/download/v0.39.2/mkr_darwin_amd64.zip'
+  sha256 '00adabd7fd98f66e5649b3944a4ad7255e171abb5906f010d24a6e078858e52f'
 
   head do
     url 'https://github.com/mackerelio/mkr.git'
