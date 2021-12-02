7ff678cc72b5da2f1c779f9024347b8867987148
Asato Wakisaka <asato.wakisaka@gmail.com>
2021-12-02 13:53:04 +0900

    provide arm64 artifacts when CPU is arm64 and is not in Rosetta 2

--- mkr.rb
+++ mkr.rb
@@ -1,8 +1,13 @@
 class Mkr < Formula
   homepage 'https://github.com/mackerelio/mkr'
   version '0.46.0'
-  url 'https://github.com/mackerelio/mkr/releases/download/v0.46.0/mkr_darwin_amd64.zip'
-  sha256 '8e013543b9fc51b0d09bf1cccf4f3a4d3f8e3546c1ec0fed31228b62dc319b55'
+  if Hardware::CPU.physical_cpu_arm64? && !Hardware::CPU.in_rosetta2?
+    url 'https://github.com/mackerelio/mkr/releases/download/v0.46.0/mkr_darwin_arm64.zip'
+    sha256 '2dc4e2bb93abcc6ca3a6e1582cf022ab8df69201f070f260c7849e2a95618063'
+  else
+    url 'https://github.com/mackerelio/mkr/releases/download/v0.46.0/mkr_darwin_amd64.zip'
+    sha256 '8e013543b9fc51b0d09bf1cccf4f3a4d3f8e3546c1ec0fed31228b62dc319b55'
+  end
 
   head do
     url 'https://github.com/mackerelio/mkr.git'
