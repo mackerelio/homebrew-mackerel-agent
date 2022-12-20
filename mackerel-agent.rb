98f27498bbd23fdbaddf8cb1961533ae278f66de
mackerelbot <mackerelbot@users.noreply.github.com>
2022-12-20 09:45:25 +0000

    Update mackerel-agent

--- mackerel-agent.rb
+++ mackerel-agent.rb
@@ -1,12 +1,12 @@
 class MackerelAgent < Formula
   homepage 'https://github.com/mackerelio/mackerel-agent'
-  version '0.73.3'
+  version '0.74.0'
   if Hardware::CPU.arm?
-    url 'https://github.com/mackerelio/mackerel-agent/releases/download/v0.73.3/mackerel-agent_darwin_arm64.zip'
-    sha256 'd6ce889e6b11efbb91e1c7bb37b7c7e747086ad0f754f88bb710c8bccca016fe'
+    url 'https://github.com/mackerelio/mackerel-agent/releases/download/v0.74.0/mackerel-agent_darwin_arm64.zip'
+    sha256 'df6ef63e91aefabcf0ee8d4975a0e3fe7039af0c5d585ba4f446c537e35845fc'
   else
-    url 'https://github.com/mackerelio/mackerel-agent/releases/download/v0.73.3/mackerel-agent_darwin_amd64.zip'
-    sha256 '3c2c0f8f92f94ef89a0db12e65403bbf488ef8e40430b488377d62248ec32094'
+    url 'https://github.com/mackerelio/mackerel-agent/releases/download/v0.74.0/mackerel-agent_darwin_amd64.zip'
+    sha256 '4d620ad9f9dd30fe73429fdcdff75aff20d8dbe27e11a484cebe1f877df9dca4'
   end
 
   head do
