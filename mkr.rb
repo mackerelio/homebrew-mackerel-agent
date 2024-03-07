aa597b079a4b9fdd1f29574c12e911079245c1d5
mackerelbot <mackerelbot@users.noreply.github.com>
2024-03-07 06:15:21 +0000

    Update mkr

--- mkr.rb
+++ mkr.rb
@@ -1,12 +1,12 @@
 class Mkr < Formula
   homepage 'https://github.com/mackerelio/mkr'
-  version '0.55.0'
+  version '0.56.0'
   if Hardware::CPU.arm?
-    url 'https://github.com/mackerelio/mkr/releases/download/v0.55.0/mkr_darwin_arm64.zip'
-    sha256 'c4a221193434eb132c3e90be1133e7dcbab85b150d63be770d81e3f8849753bb'
+    url 'https://github.com/mackerelio/mkr/releases/download/v0.56.0/mkr_darwin_arm64.zip'
+    sha256 '1ef8871d93e537717c92ebf6e1dc549cf56737996de92c3042757c4dc3fe24eb'
   else
-    url 'https://github.com/mackerelio/mkr/releases/download/v0.55.0/mkr_darwin_amd64.zip'
-    sha256 'cbb6c0117b1e8e5ccf2789ca61e926f28cfd2ac1d0ffc92f8d3184834d84b8f9'
+    url 'https://github.com/mackerelio/mkr/releases/download/v0.56.0/mkr_darwin_amd64.zip'
+    sha256 'c73e99fb7d950fe86cad677641f8d377dced61d0e61f4047e613a0a046bc0b7d'
   end
 
   head do
